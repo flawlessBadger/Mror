98e8b7b9737c3c9751a77c95c3d7d3a741778627
Jozef Ferko <jozefferko9@gmail.com>
2017-03-30 14:47:19 +0200

    a

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <HTML>
 
     <HEAD>
-        <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Muli:200,300,400,600,700,800,900&amp;subset=latin-ext" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Ek+Mukta:200,300,400,500,600,700,800&amp;subset=latin-ext" rel="stylesheet">
+        <link href="http://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+        <link href="http://fonts.googleapis.com/css?family=Raleway:300,400,500,700" rel="stylesheet">
+        <link href="http://fonts.googleapis.com/css?family=Muli:200,300,400,600,700,800,900&amp;subset=latin-ext" rel="stylesheet">
+        <link href="//fonts.googleapis.com/css?family=Ek+Mukta:200,300,400,500,600,700,800&amp;subset=latin-ext" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="styles/style.css">
         <link rel="stylesheet" type="text/css" href="styles/icons.css">
         <script src="scripts/jquery.min.js"></script>
