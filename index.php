0bad2a8d3d1199d5a5a0d71230cb5a4026bb04de
Jozef Ferko <jozefferko9@gmail.com>
2017-03-30 14:20:51 +0200

    c

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html PUBLIC "-//IETF//DTD HTML 2.0//EN">
 <HTML>
 
-    <HEAD><!-- 
+    <HEAD>
         <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Muli:200,300,400,600,700,800,900&amp;subset=latin-ext" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Ek+Mukta:200,300,400,500,600,700,800&amp;subset=latin-ext" rel="stylesheet"> -->
+        <link href="https://fonts.googleapis.com/css?family=Ek+Mukta:200,300,400,500,600,700,800&amp;subset=latin-ext" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="styles/style.css">
         <link rel="stylesheet" type="text/css" href="styles/icons.css">
         <script src="scripts/jquery.min.js"></script>
